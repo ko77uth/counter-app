152e1192e7f0852f0eb36a6d5bdb4ff6063fb941
ko77uth <izmailovmaksim11@gmail.com>
2025-10-29 21:58:28 +0300

    sssssss

--- src/styles/index.js
+++
@@ -1,9 +0,0 @@
-import './normalize.css'
-import './fonts.css'
-import './variables.css'
-import './utils.css'
-import './globals.css'
-
-import './components/button.css'
-import './components/field.css'
-import './components/modal.css'
