e4c02c4d04cf50f36df8dd2b7b4ab3c9e4c70e8f
ko77uth <izmailovmaksim11@gmail.com>
2025-10-29 21:15:05 +0300

    aaaaa

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.js'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-	React.createElement(React.StrictMode, null, React.createElement(App, null))
-)
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	base: '/counter-app/',
-	build: {
-		outDir: 'dist',
-		sourcemap: false,
-		rollupOptions: {
-			external: [],
-		},
-	},
 })
