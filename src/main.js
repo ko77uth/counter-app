5ab3514c4ce87ab6dcf93e48a36d249e61e3dc11
ko77uth <izmailovmaksim11@gmail.com>
2025-10-29 21:02:50 +0300

    aaaaaaa

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.js'
 import App from './App.jsx'
-import React from 'react'
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
-
-root.render(
-	React.createElement(StrictMode, null, React.createElement(App, null))
+createRoot(document.getElementById('root')).render(
+	React.createElement(React.StrictMode, null, React.createElement(App, null))
 )
